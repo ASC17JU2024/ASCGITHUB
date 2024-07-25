9997453aaef9d0a556147b37ed583103c1efb5f3
dev <dev@dev.com>
2024-07-25 16:13:10 +0530

    commit 😊

--- demos/ld-jdbc-crud-layered-arch/src/main/java/com/ecz/ui/App.java
+++ demos/ld-jdbc-crud-layered-arch/src/main/java/com/ecz/ui/App.java
@@ -1,18 +1,12 @@
 package com.ecz.ui;
 
-
-import java.awt.*;
-
 public class App
 {
     public static void main( String[] args )
     {
         System.out.println("Welcome to ECommerce App!");
         MenuUI menuUI = new MenuUI();
-//        menuUI.accessMenu();
-//        menuUI.addProduct();
-//        menuUI.getAllProducts();
-menuUI.updateProduct();
+        menuUI.updateProduct();
         menuUI.getAllProducts();
     }
 }
