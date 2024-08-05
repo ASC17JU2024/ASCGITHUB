95a7990e972c84dfc96d38a626beb92b3a7c0ccd
GenieFamily <IrvynsGenie@gmail.com>
2024-08-05 13:53:47 +0530

    commit 😊

---
+++ demos/spring-core/d1-mvn-setup/src/main/java/dom/ui/App.java
@@ -0,0 +1,23 @@
+package dom.ui;
+
+import dom.model.Product;
+import dom.repository.ProductRepository;
+import dom.repository.ProductRepositoryImpl;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        ProductRepository productRepository = new ProductRepositoryImpl();
+        List<Product> productList = productRepository.findAll();
+        for(int i = 0; i<productList.size(); i++){
+            System.out.println(productList.get(i));
+        }
+
+        for (Product product : productList) {
+            System.out.println(product);
+        }
+        // java 8 forEach and method-reference operator
+        productList.forEach(System.out::println);
+    }
+}
