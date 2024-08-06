12cfbedd9cd20b71bd902dd267ba52a7a8aec2f6
GenieFamily <IrvynsGenie@gmail.com>
2024-08-06 14:58:52 +0530

    commit 😊

---
+++ demos/spring-core/d7-xml-anno-ioc/src/main/java/dom/model/Product.java
@@ -0,0 +1,48 @@
+package dom.model;
+
+public class Product {
+    private String productId;
+    private String productName;
+    private float price;
+
+    public Product() {
+        super();
+    }
+
+    public Product(String productId, String productName, float price) {
+        super();
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + "]";
+    }
+
+}
